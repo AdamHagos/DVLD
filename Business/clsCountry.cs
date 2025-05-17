41cdcbcd400a792f6773528529037ea5ac559c81
AdamHagos <adamhagos10@gmail.com>
2025-05-17 03:43:55 +0300

    Add files via upload

---
+++ Business/clsCountry.cs
@@ -0,0 +1,44 @@
+﻿using DVLD_DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD_Business
+{
+    public class clsCountry
+    {
+        public int CountryID { get; private set; }
+        public string CountryName { get; private set; }
+
+        private clsCountry(int CountryID, string CountryName)
+        {
+            this.CountryID = CountryID;
+            this.CountryName = CountryName;
+        }
+        public static clsCountry GetCountryByID(int ID)
+        {
+            string CountryName = "";
+
+            bool IsFound = clsCountriesDataAccess.GetCountryByID(ID,ref CountryName);
+            if (IsFound)
+            {
+                return new clsCountry(ID, CountryName);
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public static int GetCountryID(string CountryName)
+        {
+            return clsCountriesDataAccess.GetCountryID(CountryName);
+        }
+        public static DataTable GetAllCountries()
+        {
+            return clsCountriesDataAccess.GetAllCountries();
+        }
+    }
+}
---
+++ Business/clsTestType.cs
@@ -0,0 +1,78 @@
+﻿using DVLD_DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD_Business
+{
+    public class clsTestType
+    {
+        public int TestTypeID { get; private set; }
+        public string TestTypeTitle { get; set; }
+        public string TestTypeDescription { get; set; }
+        public float TestTypeFees { get; set; }
+        public enum enTestTypes { VisionTest = 1, Written, Practical }
+        private clsTestType(int testTypeID, string testTypeTitle, string testTypeDescription, float testTypeFees)
+        {
+            TestTypeID = testTypeID;
+            TestTypeTitle = testTypeTitle;
+            TestTypeDescription = testTypeDescription;
+            TestTypeFees = testTypeFees;
+        }
+        public clsTestType()
+        {
+            TestTypeID = -1;
+            TestTypeTitle = "";
+            TestTypeDescription = "";
+            TestTypeFees = 0;
+        }
+        public static bool DeleteTestTypeByID(int ID)
+        {
+            return clsTestTypesDataAccess.DeleteTestType(ID);
+        }
+        private int _AddNewTestType()
+        {
+            return clsTestTypesDataAccess.AddNewTestType(this.TestTypeTitle, this.TestTypeDescription, this.TestTypeFees);
+        }
+        private bool _UpdateTestType()
+        {
+            return clsTestTypesDataAccess.UpdateTestType(this.TestTypeID, this.TestTypeTitle, this.TestTypeDescription, this.TestTypeFees);
+        }
+        public bool Save()
+        {
+            if (this.TestTypeID == -1)
+            {
+                this.TestTypeID = _AddNewTestType();
+                return (this.TestTypeID != -1);
+
+            }
+            else
+            {
+                return _UpdateTestType();
+            }
+        }
+        public static DataTable GetAllTestTypes()
+        {
+            return clsTestTypesDataAccess.GetAllTestTypes();
+        }
+        public static clsTestType GetTestTypeInfoByID(int ID)
+        {
+            string TestTypeTitle = "";
+            string TestTypeDescription = "";
+            float TestTypeFees = 0;
+
+            bool IsFound = clsTestTypesDataAccess.GetTestTypeByID(ID,ref TestTypeTitle, ref TestTypeDescription, ref TestTypeFees);
+            if (IsFound)
+            {
+                return new clsTestType(ID, TestTypeTitle, TestTypeDescription, TestTypeFees);
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
